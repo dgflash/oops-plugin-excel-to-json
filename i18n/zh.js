38f820be9011c67475b4c4068f95b59034591811
dgflash <dgflash@qq.com>
2024-02-10 20:46:14 +0800

    修改功能入口位置到extension/oops-framework

--- i18n/zh.js
+++ i18n/zh.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports =
 {
-    title: "Oops Framework Excel To Json",
-    description:"Oops Framework Excel 转 Json 数据插件",
+    title: "Excel转Json与Ts",
+    description: "Oops Framework Excel 转 Json 数据插件",
     excel_to_json: "Excel To Json",
 };
