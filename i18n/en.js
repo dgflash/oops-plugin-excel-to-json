a68f19f9f20c0b2919969ad05bc87bc8033b7fb7
dgflash <dgflash@qq.com>
2024-10-17 22:47:41 +0800

    修改菜单目录

--- i18n/en.js
+++ i18n/en.js
@@ -1,6 +1,8 @@
 "use strict";
 module.exports =
 {
+    name: "Oops Framework",
+    tools: "Tools",
     title: "Excel To Json And Ts",
     description: "Oops framework Excel to JSON data plug-in",
     excel_to_json: "Excel To Json",
--- i18n/zh.js
+++ i18n/zh.js
@@ -1,6 +1,8 @@
 "use strict";
 module.exports =
 {
+    name: "Oops Framework",
+    tools: "框架工具",
     title: "Excel转Json与Ts",
     description: "Oops Framework Excel 转 Json 数据插件",
     excel_to_json: "Excel To Json",
